6b69f022496c9f0e50d15b2b283e97b861848e04
reddysubbu888 <42493880+reddysubbu888@users.noreply.github.com>
2021-11-07 11:43:10 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,6 +33,54 @@ go install github.com/OJ/gobuster/v3@latest
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go get -u github.com/tomnomnom/httprobe
 go get -u github.com/ffuf/ffuf
+go get -u github.com/tomnomnom/assetfinder
+go get -v github.com/OWASP/Amass/v3/…
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+sudo apt install -y libpcap-dev
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+go get github.com/tomnomnom/waybackurls
+GO111MODULE=on go get -u -v github.com/lc/gau
+go install github.com/OJ/gobuster/v3@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go get -u github.com/tomnomnom/httprobe
+go get -u github.com/tomnomnom/fff
+go get github.com/tomnomnom/hacks/filter-resolved
+go get -u github.com/tomnomnom/hacks/gittrees
+go get github.com/tomnomnom/hacks/waybackurls
+go get -u github.com/tomnomnom/hacks/unisub
+go get -u -v github.com/lukasikic/subzy
+go install -v github.com/lukasikic/subzy
+go get -u github.com/tomnomnom/unfurl
+go get github.com/tomnomnom/burl
+go get -u github.com/tomnomnom/meg
+go get -u github.com/j3ssie/metabigor
+go get -u github.com/rverton/webanalyze
+go get -u github.com/m4ll0k/Aron
+go get github.com/Ice3man543/SubOver
+go get -u github.com/c-bata/go-prompt
+go get github.com/hahwul/websocket-connection-smuggler
+GO111MODULE=on go get -u -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+go get github.com/haccer/subjack
+go get github.com/eth0izzle/shhgit
+GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/cmd/subfinder;
+go get github.com/tiagorlampert/CHAOS;
+GO111MODULE=on go get -u github.com/projectdiscovery/chaos-client/cmd/chaos;
+GO111MODULE=on go get -u -v github.com/hahwul/dalfox;
+go get github.com/ffuf/ffuf;
+GO111MODULE=on go get -u -v github.com/lc/gau;
+go get github.com/003random/getJS;
+go get github.com/subfinder/goaltdns;
+go get github.com/OJ/gobuster;
+go get -u github.com/sensepost/gowitness;
+go get -u github.com/jaeles-project/gospider;
+go get github.com/hakluke/hakcheckurl;
+go get github.com/hakluke/hakrawler;
+go get github.com/hakluke/hakrevdns;
+go get -u github.com/tomnomnom/httprobe;
+GO111MODULE=on go get -u -v github.com/projectdiscovery/httpx/cmd/httpx;
+GO111MODULE=on go get -u -v github.com/projectdiscovery/dnsprobe;
+GO111MODULE=on go get -u -v github.com/projectdiscovery/shuffledns/cmd/shuffledns;
+
 echo "installed go tools"
 cd ~/go/bin 
 cp * /usr/bin
